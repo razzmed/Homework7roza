ab773e390132b4424a89c21fff91b4cbb96b7edc
razzmed <mederrazzakov@gmail.com>
2020-03-20 16:55:12 +0600

    V.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,67 +6,45 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<String> listA = new ArrayList<>();
         Scanner s1 = new Scanner(System.in);
         System.out.println("Введите 5 имен");
-        String strings1 = s1.nextLine();
-        String strings2 = s1.nextLine();
-        String strings3 = s1.nextLine();
-        String strings4 = s1.nextLine();
-        String strings5 = s1.nextLine();
+        listA.add(s1.nextLine());
+        listA.add(s1.nextLine());
+        listA.add(s1.nextLine());
+        listA.add(s1.nextLine());
+        listA.add(s1.nextLine());
 
-        ArrayList<String> listA = new ArrayList<>();
-        listA.add(strings1);
-        listA.add(strings2);
-        listA.add(strings3);
-        listA.add(strings4);
-        listA.add(strings5);
-
-        Iterator <String> iteratorA = listA.iterator();
-         while (iteratorA.hasNext()){
-             String nameA = iteratorA.next();
-             System.out.println(nameA);
-         }
-
-        Scanner s2 = new Scanner(System.in);
-        System.out.println("Введите еще 5 имен");
-        String strings6 = s2.nextLine();
-        String strings7 = s2.nextLine();
-        String strings8 = s2.nextLine();
-        String strings9 = s2.nextLine();
-        String strings10 = s2.nextLine();
+        Iterator<String> iteratorA = listA.iterator();
+        while (iteratorA.hasNext()) {
+            String nameA = iteratorA.next();
+            System.out.println(nameA);
+        }
+        System.out.println("_______________________________");
 
         ArrayList<String> listB = new ArrayList<>();
-        listB.add(strings6);
-        listB.add(strings7);
-        listB.add(strings8);
-        listB.add(strings9);
-        listB.add(strings10);
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Введите еще 5 имен");
+        listB.add(s2.nextLine());
+        listB.add(s2.nextLine());
+        listB.add(s2.nextLine());
+        listB.add(s2.nextLine());
+        listB.add(s2.nextLine());
 
-        Iterator <String> iteratorB = listB.iterator();
-        while (iteratorB.hasNext()){
+        Iterator<String> iteratorB = listB.iterator();
+        while (iteratorB.hasNext()) {
             String nameB = iteratorB.next();
             System.out.println(nameB);
         }
         System.out.println("_______________________________");
 
         ArrayList<String> listC = new ArrayList<>();
-        listC.add(strings1);
-        listC.add(strings10);
-        listC.add(strings2);
-        listC.add(strings9);
-        listC.add(strings3);
-        listC.add(strings8);
-        listC.add(strings4);
-        listC.add(strings7);
-        listC.add(strings5);
-        listC.add(strings6);
 
-        Iterator <String> iteratorC = listC.iterator();
-        while (iteratorC.hasNext()){
-            String nameC = iteratorC.next();
-            System.out.println(nameC);
+        for (int i = 0; i < listA.size(); i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(i));
         }
-        System.out.println("_______________________________");
+        Collections.addAll(listC);
 
         Collections.sort(listC, new Comparator<String>() {
             @Override
@@ -80,7 +58,7 @@ public class Main {
                 }
             }
         });
-        for(String str : listC){
+        for (String str : listC) {
             System.out.println(str);
         }
     }
